42afd88eab85aaccc62b7468a181f55b2fb67142
Kabuswe Mulenga <k.mulenga@sophatel.com>
2023-07-27 09:33:08 +0100

    fix: update chrome.runtime.onStartup listener

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-chrome.runtime.onStartup.addListener(function () {
+chrome.runtime.onStartup.addListener(() => {
   chrome.tabs
     .create({
       url: "http://192.168.101.38:5005/",
@@ -23,33 +23,4 @@ chrome.runtime.onStartup.addListener(function () {
         console.log(`An error occurred: ${error}`);
       }
     );
-});
-
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason === "install") {
-    chrome.tabs
-      .create({
-        url: "http://192.168.101.38:5005/",
-      })
-      .then(
-        async (tab: chrome.tabs.Tab) => {
-          if (tab?.id) {
-            await chrome.scripting.executeScript({
-              target: { tabId: tab.id },
-              func: () => {
-                window.localStorage.removeItem("token");
-              },
-            });
-
-            chrome.tabs.remove(tab.id).then(
-              () => console.log("Tab successfuly removed"),
-              (error) => console.log(`An error occurred: ${error}`)
-            );
-          }
-        },
-        (error) => {
-          console.log(`An error occurred: ${error}`);
-        }
-      );
-  }
 });
